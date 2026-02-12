003c8c471d8aad041bab002174a9dfb6f8880ab7
callisto1232 <ahmet_ka1232@hotmail.com>
2026-02-13 01:19:45 +0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,11 @@
+// duco-cnc
+
 /***********************************************************************
 GRBL MAIN FILE FOR CNC PROJECT
+Use with Universal Gcode Sender
+
+115200 Baud Rate 
+Control with serial monitor and UGS
 ***********************************************************************/
 
 #include "src/grbl/grbl.h"
